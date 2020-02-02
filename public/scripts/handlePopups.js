2e91fa545d5686d2063ce9895717063b647fc04c
aqui <aquiabuin@gmail.com>
2020-02-02 19:49:55 -0300

    fixed more css issues..

--- public/scripts/handlePopups.js
+++ public/scripts/handlePopups.js
@@ -1,13 +1,15 @@
 var popupBox = document.getElementById('popup')
 var popupImg = document.getElementById('popup-img')
+var popupTxt = document.getElementById('popup-txt')
 
 function handlePopups(url, divId) {
-  popupImg.src = url
   popupBox.classList.toggle('closed')
   if (url) {
+    popupImg.src = url
     popupImg.classList.toggle('closed')
   }
   if (url === null) {
+    popupTxt.classList.toggle('closed')
     document.getElementById(divId).classList.toggle('closed')
   }
   window.onclick = function (event) {
@@ -18,6 +20,7 @@ function handlePopups(url, divId) {
       }
       if (url === null) {
         document.getElementById(divId).classList.toggle('closed')
+        popupTxt.classList.toggle('closed')
       }
     }
   }
